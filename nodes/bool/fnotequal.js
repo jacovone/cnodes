71009165b34e26f3020e3f21e6b16294c647895d
Marco Jacovone <marco.jacovone@gmail.com>
2020-12-19 23:52:19 +0100

    Bugfix

--- nodes/bool/fnotequal.js
+++ nodes/bool/fnotequal.js
@@ -15,7 +15,7 @@ import { Comparision } from "./fcompare.js";
  */
 export class FNotEqual extends FCompare {
   constructor() {
-    super(Comparision.FNotEqual);
+    super(Comparision.NOT_EQUAL);
     this.name = "FNotEqual";
   }
 }
